8107c6fd59be0a82ecb4773f8fa259f642a7e3cd
Rodolfo <Rodolfo@192.168.242.128>
2014-12-23 01:29:02 -0100

    Nao era o o mas sim a

--- JavaApplication3/src/demotica/Alert.java
+++ JavaApplication3/src/demotica/Alert.java
@@ -9,7 +9,7 @@ import java.io.Serializable;
  */
 /**
  *
- * @author Rodolfo Melo, Henrique Melo, Xico
+ * @author Rodolfo Melo, Henrique Melo, Xica
  */
 public abstract class Alert implements Serializable{
 
